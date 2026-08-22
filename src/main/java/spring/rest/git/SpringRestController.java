7a6ff7d31d7418dacd0b24ff21742b080fe84e18
rahulbarve <rahulbarve1979@gmail.com>
2026-08-22 06:05:00 +0530

    Committing 1st time

---
+++ src/main/java/spring/rest/git/SpringRestController.java
@@ -0,0 +1,13 @@
+package spring.rest.git;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringRestController {
+
+	@GetMapping("/greet")
+	public String doGreet() {
+		return "Welcome to Spring REST with Git";
+	}
+}
---
+++ src/main/java/spring/rest/git/SpringRestGitApplication.java
@@ -0,0 +1,13 @@
+package spring.rest.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestGitApplication.class, args);
+	}
+
+}
