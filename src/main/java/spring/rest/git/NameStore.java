d9280f0469499f2c229883821549e473bf25e3e3
rahulbarve <rahulbarve1979@gmail.com>
2026-08-22 13:00:10 +0530

    Final Commit-A

---
+++ src/main/java/spring/rest/git/NameStore.java
@@ -0,0 +1,19 @@
+package spring.rest.git;
+
+import java.util.List;
+
+public class NameStore {
+	private static List<Name> allNames;
+	static {
+		Name n1 = new Name("James","Gosling");
+		Name n2 = new Name("Gavin","King");
+		Name n3 = new Name("Rod","Johnson");
+		Name n4 = new Name("Linus","Tolvald");
+		Name n5 = new Name("Ryan","Dahl");
+		allNames = List.of(n1,n2,n3,n4,n5);
+	}
+	public static List<Name> getAllNames(){
+		return allNames;
+	}
+}
+record Name(String firstName, String lastName) {}
--- src/main/java/spring/rest/git/SpringRestController.java
+++ src/main/java/spring/rest/git/SpringRestController.java
@@ -7,20 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringRestController {
-
+	
 	@GetMapping("/greet")
 	public String doGreet() {
 		return "Welcome to Spring REST using Spring Boot and Jenkins";
 	}
 	@GetMapping("/allNames")
 	public List<Name> getAllNames(){
-		Name n1 = new Name("James","Gosling");
-		Name n2 = new Name("Gavin","King");
-		Name n3 = new Name("Rod","Johnson");
-		Name n4 = new Name("Linus","Tolvald");
-		Name n5 = new Name("Ryan","Dahl");
-		return List.of(n1,n2,n3,n4,n5);
-		
+		return NameStore.getAllNames();		
 	}
 }
-record Name(String firstName, String lastName) {}
+
